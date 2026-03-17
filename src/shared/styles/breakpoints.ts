5d7bdf09d52b0609e4e14edf49073e325e6fa640
Bogdan Kurenkov <badikradik123@gmail.com>
2026-03-17 16:55:57 +0300

    updated breakpoint theme

--- src/shared/styles/breakpoints.ts
+++ src/shared/styles/breakpoints.ts
@@ -1,5 +1,23 @@
 export const breakpoints = {
-  mobile: "320px",
-  tablet: "768px",
-  desktop: "1024px",
+  xs: "480px",
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px",
+} as const;
+
+export const devices = {
+  xs: `@media (max-width: ${breakpoints.xs})`,
+  sm: `@media (max-width: ${breakpoints.sm})`,
+  md: `@media (max-width: ${breakpoints.md})`,
+  lg: `@media (max-width: ${breakpoints.lg})`,
+  xl: `@media (max-width: ${breakpoints.xl})`,
+  "2xl": `@media (max-width: ${breakpoints["2xl"]})`,
+
+  minSm: `@media (min-width: ${breakpoints.sm})`,
+  minMd: `@media (min-width: ${breakpoints.md})`,
+  minLg: `@media (min-width: ${breakpoints.lg})`,
+  minXl: `@media (min-width: ${breakpoints.xl})`,
+  min2xl: `@media (min-width: ${breakpoints["2xl"]})`,
 };
